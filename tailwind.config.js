90daa806b0f1d9d147509d001d5777a9cd8326a8
Johanne <johannemay.jm@gmail.com>
2021-06-13 13:08:58 +0200

    init graphical chart

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: [
     './*.html',
@@ -6,6 +8,18 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'primary-dark-blue': '#2d304e',
+      'primary-lime-green': '#22d555',
+      'primary-bright-cyan': '#1cb7dc',
+      'neutral-grayish-blue': '#9698a7',
+      'neutral-light-grayish-blue': '#f3f4f6',
+      'neutral-light-gray': '#fafafa',
+    }),
+    fontFamily: {
+      'sans': ['Public Sans', ...defaultTheme.fontFamily.sans],
+    }
   },
   variants: {
     extend: {},
